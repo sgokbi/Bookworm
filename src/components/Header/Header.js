5d532b6be3dcb03916f95ce6e76443394cff98f2
SagorikaKubi <sagorika.tech@gmail.com>
2021-04-02 22:31:24 +0600

    add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className="header">
             <nav>
-                <Link className="logo" to="">Knowledge Achieve</Link>
+                <Link className="logo" to="">Bookworms</Link>
                 <Link to="/home">Home</Link>
                 <Link to="/orders">Orders</Link>
                 <Link to="/admin">Admin</Link>
